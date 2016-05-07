5ceb546e172d0f3a821b78744b0fd02874274571
Russell Wolf <rwolf@intrepid.io>
2016-05-07 11:11:31 -0400

    Use VectorDrawable

--- app/src/main/java/io/intrepid/russell/swirl/MainActivity.java
+++ app/src/main/java/io/intrepid/russell/swirl/MainActivity.java
@@ -1,26 +1,40 @@
 package io.intrepid.russell.swirl;
 
-import android.graphics.Color;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
+import butterknife.BindDrawable;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.image)
+    ImageView imageView;
+
+    @BindDrawable(R.drawable.swirl_anim)
+    Drawable swirlDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        final ImageView image = (ImageView) findViewById(R.id.image);
-        final SwirlDrawable drawable = new SwirlDrawable(getResources().getDisplayMetrics().density, Color.RED);
-        //noinspection ConstantConditions
-        image.setImageDrawable(drawable);
-        image.setOnClickListener(new View.OnClickListener() {
+        final AnimatedVectorDrawable animatedVectorDrawable = (AnimatedVectorDrawable) swirlDrawable;
+        imageView.setImageDrawable(animatedVectorDrawable);
+        imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                drawable.toggleAnimation();
+                if (animatedVectorDrawable.isRunning()) {
+                    animatedVectorDrawable.stop();
+                } else {
+                    animatedVectorDrawable.start();
+                }
             }
         });
     }
