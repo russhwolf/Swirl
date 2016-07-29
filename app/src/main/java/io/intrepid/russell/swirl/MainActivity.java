2e742736d5d851f433808f99a3d0c6683aab6f4b
Russell Wolf <rwolf@intrepid.io>
2016-07-29 17:39:49 -0400

    Support vectors (w/o working repeat)

--- app/src/main/java/io/intrepid/russell/swirl/MainActivity.java
+++ app/src/main/java/io/intrepid/russell/swirl/MainActivity.java
@@ -1,13 +1,11 @@
 package io.intrepid.russell.swirl;
 
-import android.graphics.drawable.AnimatedVectorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
-import butterknife.BindDrawable;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -16,16 +14,13 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.image)
     ImageView imageView;
 
-    @BindDrawable(R.drawable.swirl_anim)
-    Drawable swirlDrawable;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        final AnimatedVectorDrawable animatedVectorDrawable = (AnimatedVectorDrawable) swirlDrawable;
+        final AnimatedVectorDrawableCompat animatedVectorDrawable = AnimatedVectorDrawableCompat.create(this, R.drawable.swirl_anim);
         imageView.setImageDrawable(animatedVectorDrawable);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
